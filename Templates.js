cf08bcc9f9c9bb5a9c2d0ad40ee8a4aec43ced32
spelerin <45951516+spelerin@users.noreply.github.com>
2026-01-19 23:28:15 +0300

    Update Templates.js

--- Templates.js
+++ Templates.js
@@ -118,9 +118,10 @@ import { auth } from './firebase-config.js';
 							<button id="close-tags" class="px-2 py-1 text-[10px] font-bold hover:bg-white rounded transition-all uppercase text-slate-400">Kapat</button>
 						</div>
 
-						<button class="bg-blue-600 text-white p-2 md:px-4 md:py-2 rounded-lg text-sm font-semibold hover:bg-blue-700 transition-all flex-shrink-0">
-							<span class="hidden md:inline">Ekle</span>
-							<svg class="w-5 h-5 md:hidden" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path></svg>
+						<button data-action="add-new-note" class="bg-blue-600 text-white px-6 py-2 rounded-xl font-bold flex items-center gap-2 hover:bg-blue-700 transition-all">
+						    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+						        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
+						    </svg>
 						</button>
 					</div>
 				</header>
@@ -631,6 +632,7 @@ EditFileRow(file, index) {
 		
 
 };
+
 
 
 
