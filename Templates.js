4b86f5cc479805f59a39483a5a87aa8f06f98cc4
spelerin <45951516+spelerin@users.noreply.github.com>
2026-01-20 00:48:11 +0300

    Update Templates.js

--- Templates.js
+++ Templates.js
@@ -430,7 +430,7 @@ WelcomeView() {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path>
                 </svg>
             </div>
-            <h2 class="text-xl font-bold text-slate-800 mb-2">Bilgi Üssüne Hoş Geldiniz</h2>
+            <h2 class="text-xl font-bold text-slate-800 mb-2">PYU-Not Uygulamasına Hoş Geldiniz</h2>
             <p class="text-slate-500 max-w-sm leading-relaxed text-sm">
                 Okumak istediğiniz makaleyi sol taraftaki listeden seçebilir veya yukarıdaki arama kutusunu kullanabilirsiniz.
             </p>
@@ -632,6 +632,7 @@ EditFileRow(file, index) {
 		
 
 };
+
 
 
 
