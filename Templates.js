525c89647f6211ed7c5978b3229046a9b56cc519
spelerin <45951516+spelerin@users.noreply.github.com>
2026-01-20 04:02:33 +0300

    Update Templates.js

--- Templates.js
+++ Templates.js
@@ -123,7 +123,7 @@ import { auth } from './firebase-config.js';
 				<main id="content-area" class="flex-grow flex flex-col h-full overflow-hidden" data-layout="hidden">
 					
 					<section id="tag-pool" class="bg-slate-50 border-b border-slate-200 transition-all duration-500 ease-in-out overflow-y-auto">
-						<div class="p-6 md:p-10 text-center">
+						<div class="py-4 px-6 text-center">
 							<div class="flex flex-wrap justify-center gap-x-6 gap-y-4 max-w-4xl mx-auto">
 								<button class="text-xl font-bold text-blue-600 hover:underline">#javascript</button>
 								<button class="text-sm font-medium text-slate-500 hover:text-blue-600">#ik</button>
@@ -745,6 +745,7 @@ TagPool(entries, currentLayout, selectedTags, searchTerm) {
 		
 
 };
+
 
 
 
